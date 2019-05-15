ebaabdac8b367d34bbc95f756f37fbb8668233dc
Masmics <cherlocker3113@gmail.com>
2019-05-15 16:07:08 -0700

    Text Formatter works as expected, tests passing, stretch goal is figlet integration

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -1,14 +1,25 @@
 import React, { PureComponent } from 'react';
+//import figlet from 'figlet';
 
 export default class PrintText extends PureComponent {
   state = {
     text: '',
     color: 'black'
+    //    font: 'banner3-D'
   };
 
   handleSubmit = event => {
     event.preventDefault();  
   }
+
+  // formatText = () => {
+  //   const { text, font } = this.state;
+  //   figlet.text(text, {
+  //     font: font
+  //   }, (err, result) => {
+  //     this.setState({ text: result });
+  //   });
+  // }
 
   handleTextEntry = ({ target }) => {
     this.setState({ text: target.value });
@@ -18,16 +29,26 @@ export default class PrintText extends PureComponent {
     this.setState({ color: target.value });
   }
 
+  // handleFontChange = ({ target }) => {
+  //   this.setState({ font: target.value });
+  // }
+
   render() {
     const {
       text,
       color
+      //      font
     } = this.state;
+
+    // const fontOptions = ['basic', 'broadway', 'doh', 'isometric4', 'poison'].map(font => {
+    //   return <option key={font} value={font}>{font}</option>;
+    // });
 
     return (
       <>
         <form onSubmit={this.handleSubmit}>
           <input name="text" value={text} onChange={this.handleTextEntry} />
+      
           <select name="color" value={color} onChange={this.handleColorChange}>
             <option value="red">Red</option>
             <option value="orange">Orange</option>
@@ -44,3 +65,5 @@ export default class PrintText extends PureComponent {
     );
   }
 }
+
+//<select name="font" value={font} onChange={this.formatText}>{fontOptions}</select>
--- src/components/TextFormatter.test.js
+++ src/components/TextFormatter.test.js
@@ -8,9 +8,9 @@ describe('TextFormatter', () => {
     expect(wrapper).toMatchSnapshot;
   });
 
-  // it('can change to the selected text color', () => {
-  //   const wrapper = shallow(<TextFormatter />);
-  //   wrapper.find('select').at(0).simulate('click');
-  //   expect(wrapper.state('color')).toEqual('red');
-  // });
+  it('can render the default text color', () => {
+    const wrapper = shallow(<TextFormatter />);
+    wrapper.find('select').at(0).simulate('select');
+    expect(wrapper.state('color')).toEqual('black');
+  });
 });
