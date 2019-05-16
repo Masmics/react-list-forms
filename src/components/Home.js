67499e572118778f1b7ee083c35785bcb23f4efb
Masmics <cherlocker3113@gmail.com>
2019-05-15 19:16:43 -0700

    pausing

---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+function Home {
+  console.log('I am home');
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React from 'react'; // { Component }
+import PropTypes from 'prop-types';
+// import Home from './Home';
+// import About from './About';
+// import Blog from './Blog';
+// import Contact from './Contact';
+import styles from './Sidebar.css';
 
+// <Sidebar />
 
+function Sidebar({ children }) {
+  return (
+    <nav className={styles.Sidebar}>
+      {children}
+    </nav>
+  );
+}
+
+Sidebar.propTypes = {
+  children: PropTypes.node
+};
+
+export default Sidebar;
