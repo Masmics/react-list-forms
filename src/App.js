68c107323841173bb3b156d9a66336bde42b7599
Masmics <cherlocker3113@gmail.com>
2019-05-17 21:34:10 -0700

    error app complete, random true or false on page refresh feature added, tests passing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,53 @@
 import React from 'react';
+//import PropTypes from 'prop-types';
+import Bad from './components/Bad';
+import Good from './components/Good';
+import ErrorBoundary from './components/ErrorBoundary';
 
+function App() {
 
+  // class Counter extends Component {
+  //   static propTypes = {
+  //     count: PropTypes.number
+  //   }
+
+  //   state = {
+  //     count: 0
+  //   }
+
+  //   onClick(e) {
+  //     this.setState({
+  //       count: this.state.count + 1
+  //     });
+  //     console.log(e);
+  //   }
+
+  //   render() {
+  //     return (
+  //       this.state.count,
+        
+  //       <div>
+  //         <button onClick={this.onClick.bind(this)}>Good or Bad?</button>
+  //       </div>
+  //     );
+  //   }
+  // }
+
+  //function yeaOrNay(tally) {
+  const yeaOrNay = Math.floor((Math.random() * 10) + 1);
+  if(yeaOrNay % 2 === 0) {
+    return (
+      <ErrorBoundary>
+        <Good />
+      </ErrorBoundary>
+    );
+  } else {
+    return (
+      <ErrorBoundary>
+        <Bad />
+      </ErrorBoundary>
+    );
+  }
+}
+
+export default App;
